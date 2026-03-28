a1ba3cb45f1fc157077bac1c7c07f0566422916b
Sultan2403 <sultanremilekun@gmail.com>
2026-03-28 15:36:34 +0100

    changed image fallback to use local asset

--- frontend/src/shared/imageFallbacks.js
+++ frontend/src/shared/imageFallbacks.js
@@ -1,5 +1,7 @@
+import defaultProfilePic from "../images/default_profile_avatar.svg";
+
 export function getDefaultProfilePic() {
-  return "https://imgs.search.brave.com/OrD16cB7BwEj3DZfFr7OHRQyABsYifutWhl975vIvII/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93d3cu/cG5nYXJ0cy5jb20v/ZmlsZXMvMTAvRGVm/YXVsdC1Qcm9maWxl/LVBpY3R1cmUtUE5H/LUltYWdlLVRyYW5z/cGFyZW50LUJhY2tn/cm91bmQucG5n";
+  return defaultProfilePic;
 }
 
 export function resolveProfilePic(profilePic, seed) {
